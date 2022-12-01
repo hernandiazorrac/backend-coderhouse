cf37e810d6e95563a1bc98f4fdaa86070b739ca7
Hernán Díaz Orrac <hernandiazorrac@gmail.com>
2022-11-30 21:46:46 -0300

    se imprime el array con productos y se selecciona un producto por ID en la url relativa

--- productManager/express/src/app.js
+++ productManager/express/src/app.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const fs = require('fs')
+const fileName = "../productManager/fileSystem/products.json"
+const format = "utf-8"
+const products = fs.readFileSync(fileName, format)
+
 const app = express()
 
-app.get('/products', (req, res) => {
-    res.send('AAAAAAAAAA')
+app.get('/products', (req, res) => res.send(products))
+app.get('/products/:pid', (req, res) => {
+    const productID = req.params.pid 
+    const parsedProducts = JSON.parse(products)
+    const product = parsedProducts.find(product => product.id == productID)
+    
+    if(!product) res.send(`<h2>Error: Product not found.</h2>`)
+    else res.send(product)
 })
 
 app.listen(8080, () => console.log('Server is running ...'))
