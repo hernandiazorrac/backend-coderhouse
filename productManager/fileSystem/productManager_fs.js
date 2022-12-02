e4b807cb9bd6ac200fe799ff6370520da80dd98a
Hernán Díaz Orrac <hernandiazorrac@gmail.com>
2022-12-01 23:02:19 -0300

    agregados más productos

--- productManager/fileSystem/productManager_fs.js
+++ productManager/fileSystem/productManager_fs.js
@@ -76,7 +76,12 @@ async function run(){
     await manager.addProduct('producto 2', 'descripción 2', 2500, 'N/A', 20, 'abc456')
     await manager.addProduct('producto 3', 'descripción 3', 3500, 'N/A', 30, 'abc789')
     await manager.addProduct('producto 4', 'descripción 4', 4500, 'N/A', 40, 'def123')
-    await manager.addProduct('producto 5', 'descripción 5', 5500, 'N/A', 50, 'def123')
+    await manager.addProduct('producto 5', 'descripción 5', 5500, 'N/A', 50, 'def456')
+    await manager.addProduct('producto 6', 'descripción 6', 6500, 'N/A', 60, 'def789')
+    await manager.addProduct('producto 7', 'descripción 7', 7500, 'N/A', 70, 'ghi123')
+    await manager.addProduct('producto 8', 'descripción 8', 8500, 'N/A', 80, 'ghi456')
+    await manager.addProduct('producto 9', 'descripción 9', 9500, 'N/A', 90, 'ghi789')
+    await manager.addProduct('producto 10', 'descripción 10', 10500, 'N/A', 100, 'jkl123')
     await manager.deleteProduct(2);
 
     await manager.updateProduct(4, "title", "PRODUCTO ACTUALIZADO")
