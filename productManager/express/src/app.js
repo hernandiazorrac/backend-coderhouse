974596db85f3c71163106cbcf02cee056906f0ba
Hernán Díaz Orrac <hernandiazorrac@gmail.com>
2022-12-05 15:05:36 -0300

    agregada query para limitar los productos que se imprimen

--- productManager/express/src/app.js
+++ productManager/express/src/app.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const fs = require('fs')
-const fileName = "../productManager/fileSystem/products.json"
+const fileName = "../../productManager/fileSystem/products.json"
 const format = "utf-8"
 const products = fs.readFileSync(fileName, format)
+const parsedProducts = JSON.parse(products)
 
 const app = express()
 
-app.get('/products', (req, res) => res.send(products))
+app.get('/products', (req, res) => {
+    const limit = req.query.limit
+    
+    if(limit){
+        res.send(parsedProducts.slice(0,limit))
+    }else{
+        res.send(parsedProducts)
+    }
+})
 app.get('/products/:pid', (req, res) => {
     const productID = req.params.pid 
-    const parsedProducts = JSON.parse(products)
+    
     const product = parsedProducts.find(product => product.id == productID)
     
     if(!product) res.send(`<h2>Error: Product not found.</h2>`)
